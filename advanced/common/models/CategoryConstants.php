d386e1435f9fc192578e11598982f899b64fcbd8
Lanegan <null@binaryworld.ru>
2016-02-26 09:50:24 +0300

    Специальный класс, для хранения констант. Названия категорий у нас - константы. Для того, чтобы было удобно ими управлять, создал отдельный класс.

---
+++ advanced/common/models/CategoryConstants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lanegan
+ * Date: 26.02.2016
+ * Time: 9:43
+ */
+
+namespace common\models;
+
+
+class CategoryConstants
+{
+    public static $MANUAL = 'Руководства';
+    public static $NEWS = 'Новости';
+    public static $ADDITIONS = 'Дополнения';
+    public static $BLUE_TWEETS = 'Blue Tweets';
+}
